097655660162cc68b0af58d895af5e3b541a5bbb
Clement Lork <clement_lork@mymail.sutd.edu.sg>
2018-02-20 13:55:01 +0800

    fixed_url

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/example' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    //'url' : 'mongodb://localhost:27017/example' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'url' : process.env.MONGOLAB_URI // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
