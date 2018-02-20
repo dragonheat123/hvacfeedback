b213a7465b4f80c955cc61b6eb39262085d510b2
Clement Lork <clement_lork@mymail.sutd.edu.sg>
2018-02-20 14:11:38 +0800

    fixed_url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
 // configuration ===============================================================
-var mgurl = process.env.MONGOLAB_URI;
+var mgurl = process.env.MONGODB_URI;
 //var mgurl = 'mongodb://localhost:27017/example';
 
 mongoose.connect(mgurl); // connect to our database
